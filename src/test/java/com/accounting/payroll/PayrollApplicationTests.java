2af801ac8ccec6b346ee532adc94b4f0f82d6508
laurencetuchin <laurencetuchin@gmail.com>
2022-04-06 14:04:10 +1000

    Initial commit

---
+++ src/test/java/com/accounting/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accounting.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
